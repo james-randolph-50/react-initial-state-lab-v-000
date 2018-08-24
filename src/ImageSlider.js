5c3ce9831808adbe4bae341de696a94ddcbb3c1b
James Randolph <james.randolph2016@gmail.com>
2018-08-24 21:15:30 +0000

    automatically backed up by learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,12 @@
-// your ImageSlider code here!
+import React from 'react';
+
+export default class ImageSlider extends React.Component {
+  
+  constructor() {
+    super()
+    
+    this.state = {
+      curren
+    }
+  }
+}
